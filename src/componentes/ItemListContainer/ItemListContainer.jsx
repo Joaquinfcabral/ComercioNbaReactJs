c2d5522543dfced50b10bfd8be6d2d92757d3a65
Joaquin Fernandez Cabral <joaquinfcabral@gmail.com>
2022-07-27 23:22:37 -0300

    Primer subida al repositorio, creación de menu y Leanding

---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const ItemListContainer = () =>{
+
+    return(
+        <>
+        <h1>El único comercio de NBA en la Argentina</h1>
+        </>
+    )
+
+}
+
+export default ItemListContainer
---
+++ src/componentes/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../styles/Navbar.css'
+import CartWidget from '../cartwidget/CartWidget'
+
+
+const NavBar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bgNav">
+                <div className="container-fluid">
+                    <a className="navbar-brand colorL" href="#" > Panda's</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className=" ulPosicion collapse navbar-collapse" id="navbarNav">
+                        <ul className="ul navbar-nav">
+                            <li className=" nav-item">
+                                <a className="nav-link colorL active" aria-current="page"> T-shirts </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link colorL active"> Hoodies</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link colorL active"> Cups</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <CartWidget />
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
