d58a9bb336a4ae7dd44bac6be013eb47c1a691b4
Joaquin Fernandez Cabral <joaquinfcabral@gmail.com>
2022-09-06 21:18:58 -0300

    Haciendo funcionar el Cart

--- src/CartContext/CartContext.js
+++ src/CartContext/CartContext.js
@@ -8,15 +8,38 @@ const CartContextProvider = ({children}) => {
     const [carrito, setCarrito] = useState([])
 
     const addToCart = ( item ) => {
+        const itemInCart = carrito.find((itemInCart)=>itemInCart.id === item.id);
+        if(itemInCart){
+            const nuevoCarrito = carrito.map((itemInCart) => {
+                if(itemInCart.id === item.id){
+                    return{...item, count: itemInCart.count + item.count, };
+                }else {
+                    return itemInCart
+                    
+                }
+            });
+            setCarrito(nuevoCarrito)
+        }else {
+            setCarrito({...carrito, item})
+        }
         setCarrito ( [
             ...carrito,
             item        
         ] );
     }
 
+    const removeFromCart = (id) => {
+        setCarrito( carrito.filter((item)=>item.id !== id))
+
+        
+    }
+
+    const removeAll = () => {
+        setCarrito({})
+    }
 
         return ( 
-        <CartContext.Provider value = {{carrito, addToCart}}> 
+        <CartContext.Provider value = {{carrito, addToCart, removeFromCart, removeAll}}> 
 
         {children} 
         
